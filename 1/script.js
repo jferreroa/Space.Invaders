385df922d0c08e0c38c6f7af9b8bdfc4ca833b51
Jaime <raferrero10@outlook.es>
2022-02-04 12:44:17 +0100

    movimiento naves y personaje

--- 1/script.js
+++ 1/script.js
@@ -22,6 +22,7 @@ const crearNave = () => {
   nave.className = 'nave'
   const left = Math.floor(Math.random() * 1000) + 500
   nave.style.left = `${left}px`
+  nave.style.top = "0px"
   nave.innerHTML = 'NAVE'
   nave.addEventListener('click', () => {
     espacio.removeChild(nave)
@@ -35,12 +36,36 @@ const crearPersonaje = () => {
   console.log("Creando personaje");
   let personaje = document.createElement("div")
   personaje.className = "personaje"
-  //personaje.style.left = window.innerWidth/2 + 'px'
-  //personaje.style.top = "80%"
-  personaje.innerHTML = "personaje"
+  personaje.style.left = window.innerWidth/2 
+  personaje.style.top = window.innerHeight - 200
+  personaje.innerHTML = "PERSONAJE"
+
+
+
   personaje.addEventListener('click', () => {
     personaje.style.display = 'none'
   })
+
+  document.addEventListener('keydown', (event) => {
+    console.log("movimiento personaje")
+    switch (event.key) {
+      case "ArrowRight":
+        console.log("derecha" + document.querySelector(".personaje").style.left)
+        
+        document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) + 90 + 'px'
+        break;
+  
+      case "ArrowLeft":
+        console.log("izquierda")
+        document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) - 90 + 'px'
+        break;
+    }
+  })
+
+
+
+
+
   const espacio = document.querySelector(".espacio")
   espacio.appendChild(personaje)
 
@@ -54,14 +79,36 @@ window.onload = () => {
   crearNave()
   crearNave()
   crearNave()
-
   
-    setInterval(() => {
-      const naves = document.querySelectorAll(".nave")
-      if(naves.length < 4){
-        crearNave();
-      }
+  
+
+
+  setInterval(() => {
+    const naves = document.querySelectorAll(".nave")
+    if (naves.length < 4) {
+      crearNave();
+    }
   }, 300)
+
+  setInterval(() => {
+    const naves = document.querySelectorAll(".nave")
+    naves.forEach((nave,index) => {
+      console.log("movimiento nave" + nave.style.top +  index)
+      nave.style.top = parseInt(nave.style.top) + Math.floor(Math.random() * 30) + 10 + 'px'
+      console.log("new style top" + nave.style.top)
+    })
+  },100)
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
