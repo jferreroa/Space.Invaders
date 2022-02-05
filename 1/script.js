b2217c6422f912eaa1c06f16247581bfd2404d03
Jaime <raferrero10@outlook.es>
2022-02-05 19:12:43 +0100

    movimiento balas

--- 1/script.js
+++ 1/script.js
@@ -1,42 +1,40 @@
-const move = () => {
-  //alert("no pinchhees");
-  let elem = document.getElementById("titulo")
-  if (elem.style.top == "30%") {
-    elem.style.top = "50%"
-    elem.innerHTML = "Hola"
-  } else {
-    elem.style.top = "30%";
-    //elem.textContent = "Adios"
-    elem.innerHTML = "adios"
-  }
-
-  console.log(parseInt(elem.style.top))
-}
-
-
 
 
 const crearNave = () => {
   console.log("Creando nave")
   let nave = document.createElement('div')
   nave.className = 'nave'
-  const left = Math.floor(Math.random() * 1000) + 500
+  const left = Math.floor(Math.random() * 1300) + (0)
   nave.style.left = `${left}px`
   nave.style.top = "0px"
   nave.innerHTML = 'NAVE'
   nave.addEventListener('click', () => {
     espacio.removeChild(nave)
   })
-
   const espacio = document.querySelector('.espacio')
   espacio.appendChild(nave)
 }
+
+
+
+const crearBala = () => {
+  console.log("creando bala")
+  let bala = document.createElement("div")
+  const personaje = document.querySelector(".personaje")
+  bala.style.left = parseInt(personaje.style.left) + 70 + "px"
+  bala.style.top = parseInt(personaje.style.top) - 30 +  "px"
+  bala.className = "bala"
+  bala.innerHTML = "bala"
+  const espacio = document.querySelector(".espacio")
+  espacio.appendChild(bala)
+}
+
 
 const crearPersonaje = () => {
   console.log("Creando personaje");
   let personaje = document.createElement("div")
   personaje.className = "personaje"
-  personaje.style.left = window.innerWidth/2 
+  personaje.style.left = window.innerWidth / 2
   personaje.style.top = window.innerHeight - 200
   personaje.innerHTML = "PERSONAJE"
 
@@ -51,14 +49,22 @@ const crearPersonaje = () => {
     switch (event.key) {
       case "ArrowRight":
         console.log("derecha" + document.querySelector(".personaje").style.left)
-        
+
         document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) + 90 + 'px'
         break;
-  
+
       case "ArrowLeft":
         console.log("izquierda")
         document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) - 90 + 'px'
         break;
+
+
+      case " ": 
+
+        crearBala()
+        console.log("ESPACIOOOOOOOOO")
+        break;
+
     }
   })
 
@@ -73,31 +79,56 @@ const crearPersonaje = () => {
 }
 
 window.onload = () => {
-
   crearPersonaje()
   crearNave()
   crearNave()
   crearNave()
   crearNave()
-  
-  
 
 
+
+  //crear naves
   setInterval(() => {
+    console.log("cerar nave")
     const naves = document.querySelectorAll(".nave")
     if (naves.length < 4) {
       crearNave();
     }
+
+    naves.forEach(nave => {
+      if (parseInt(nave.style.top) > 550) {
+        const espacio = document.querySelector('.espacio')
+        espacio.removeChild(nave)
+      }
+    })
   }, 300)
 
+  //movimiento naves
   setInterval(() => {
     const naves = document.querySelectorAll(".nave")
-    naves.forEach((nave,index) => {
-      console.log("movimiento nave" + nave.style.top +  index)
-      nave.style.top = parseInt(nave.style.top) + Math.floor(Math.random() * 30) + 10 + 'px'
+    naves.forEach((nave, index) => {
+      console.log("movimiento nave" + nave.style.top + index)
+      nave.style.top = parseInt(nave.style.top) +  5 + 'px'
       console.log("new style top" + nave.style.top)
     })
-  },100)
+  }, 100)
+
+
+
+//movimiento balas 
+setInterval(() => {
+  const balas = document.querySelectorAll(".bala")
+  balas.forEach((bala, index) => {
+    bala.style.top = parseInt(bala.style.top) - 20 + 'px'
+    if(parseInt(bala.style.top) <  100){
+      const espacio = document.querySelector('.espacio')
+      espacio.removeChild(bala)
+    }
+  })
+
+ 
+
+},100)
 
 
 
