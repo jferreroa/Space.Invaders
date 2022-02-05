425825747b946f3b0f057126665fecfac02bbacd
Jaime <raferrero10@outlook.es>
2022-02-06 00:54:52 +0100

    colisiones

--- 1/script.js
+++ 1/script.js
@@ -1,5 +1,7 @@
-
-
+let marcador = 0;
+function docWrite(variable) {
+  document.write(variable);
+}
 const crearNave = () => {
   console.log("Creando nave")
   let nave = document.createElement('div')
@@ -22,7 +24,7 @@ const crearBala = () => {
   let bala = document.createElement("div")
   const personaje = document.querySelector(".personaje")
   bala.style.left = parseInt(personaje.style.left) + 70 + "px"
-  bala.style.top = parseInt(personaje.style.top) - 30 +  "px"
+  bala.style.top = parseInt(personaje.style.top) - 30 + "px"
   bala.className = "bala"
   bala.innerHTML = "bala"
   const espacio = document.querySelector(".espacio")
@@ -38,29 +40,21 @@ const crearPersonaje = () => {
   personaje.style.top = window.innerHeight - 200
   personaje.innerHTML = "PERSONAJE"
 
-
-
-  personaje.addEventListener('click', () => {
-    personaje.style.display = 'none'
-  })
-
   document.addEventListener('keydown', (event) => {
     console.log("movimiento personaje")
     switch (event.key) {
       case "ArrowRight":
         console.log("derecha" + document.querySelector(".personaje").style.left)
 
-        document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) + 90 + 'px'
+        document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) + 85 + 'px'
         break;
 
       case "ArrowLeft":
         console.log("izquierda")
-        document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) - 90 + 'px'
+        document.querySelector(".personaje").style.left = parseInt(document.querySelector(".personaje").style.left) - 85 + 'px'
         break;
 
-
-      case " ": 
-
+      case " ":
         crearBala()
         console.log("ESPACIOOOOOOOOO")
         break;
@@ -68,13 +62,8 @@ const crearPersonaje = () => {
     }
   })
 
-
-
-
-
   const espacio = document.querySelector(".espacio")
   espacio.appendChild(personaje)
-
 
 }
 
@@ -101,34 +90,57 @@ window.onload = () => {
         espacio.removeChild(nave)
       }
     })
-  }, 300)
+  }, 400)
 
   //movimiento naves
   setInterval(() => {
     const naves = document.querySelectorAll(".nave")
     naves.forEach((nave, index) => {
       console.log("movimiento nave" + nave.style.top + index)
-      nave.style.top = parseInt(nave.style.top) +  5 + 'px'
+      nave.style.top = parseInt(nave.style.top) + 5 + 'px'
       console.log("new style top" + nave.style.top)
     })
   }, 100)
 
 
 
-//movimiento balas 
-setInterval(() => {
-  const balas = document.querySelectorAll(".bala")
-  balas.forEach((bala, index) => {
-    bala.style.top = parseInt(bala.style.top) - 20 + 'px'
-    if(parseInt(bala.style.top) <  100){
-      const espacio = document.querySelector('.espacio')
-      espacio.removeChild(bala)
-    }
-  })
+  //movimiento balas 
+  setInterval(() => {
+    const balas = document.querySelectorAll(".bala")
+    const naves = document.querySelectorAll(".nave")
+    const espacio = document.querySelector('.espacio')
 
- 
+    balas.forEach((bala, index) => {
+      bala.style.top = parseInt(bala.style.top) - 20 + 'px'
+      if (parseInt(bala.style.top) < 100) {
+        espacio.removeChild(bala)
+      }
+      naves.forEach(nave => {
+        let posBala = {
+          altura: parseInt(bala.style.top),
+          anchura: parseInt(bala.style.left),
+          XX: parseInt(bala.style.left) + 20,
+          YY: parseInt(bala.style.top) + 30
+        };
+        let posNave = {
+          altura: parseInt(nave.style.top),
+          anchura: parseInt(nave.style.left),
+          XX: parseInt(nave.style.left) + 100,
+          YY: parseInt(nave.style.top) + 20
+        };
+        if (posBala.anchura <= posNave.XX && posBala.XX >= posNave.anchura && posBala.YY >= posNave.altura && posBala.altura <= posNave.YY) {
 
-},100)
+          console.log("CHOCAAAAAAAAN")
+          espacio.removeChild(nave)
+          espacio.removeChild(bala)
+            marcador++
+        }
+      })
+
+
+
+    })
+  }, 10)
 
 
 
